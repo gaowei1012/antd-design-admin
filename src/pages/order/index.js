562f246d5466f4596147411aba1d7b6ef81078dc
apple <apple@appledeMacBook-Air.local>
2019-01-20 21:10:49 +0800

    添加order页面

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Form, Button, Input, Table } from 'antd';
 import './index.less';
 
 export default class Order extends React.Component {
     render() {
         return(
-            <div>order</div>
+            <div>
+                <div className='topFilterWrap crad-wrap'>
+                    <FilterWrap />
+                </div>
+                <div>
+                    <Button className='operation-buttons'>订单详情</Button>
+                </div>
+                <Table
+
+                />
+            </div>
         )
     }
 }
+
+class FilterWrap extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+FilterWrap = Form.create({})(FilterWrap);
